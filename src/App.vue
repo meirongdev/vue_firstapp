6d218797329984e402463d13e3181d013471de4d
Meirong <meirongdev@gmail.com>
2024-08-22 16:12:42 +0800

    feat: form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
+  <MyForm/>
   <MyCounters :nb="3"/>
 </template>
 
-<script >
-import MyCounters from './components/MyCounters.vue'
+<script setup>
+import MyCounters from './components/MyCounters.vue';
+import MyForm from './components/MyForm.vue';
 
-export default {
-  name: 'App',
-  components: {
-    MyCounters: MyCounters
-  }
-}
+// export default {
+//   name: 'App',
+//   components: {
+//     MyCounters: MyCounters
+//   }
+// }
 </script>
 
 <style scoped>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { ref } from 'vue';
+  const name = ref('');
+  let dates = [];
+  for (let year=2024; year > 1900; year--) {
+    dates.push(year);
+  }
+  const birthYear = ref('');
+  const maritalStatus = ref("");
+</script>
+
+<template>
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" v-model="name" />
+    <label for="birthYear">Birth Year:</label>
+    <select id="birthYear" v-model="birthYear">
+      <option v-for="date in dates" :key="date" :value="date">{{ date }}</option>
+    </select>
+
+  Marital Status:
+  <input type="radio" id="single" name="maritalStatus" value="single" v-model="maritalStatus">
+  <label for="single">Single</label>
+  <input type="radio" id="married" name="maritalStatus" value="married" v-model="maritalStatus">
+  <label for="married">Married</label>
+  </form>
+  <h3> Reactive Variables</h3>
+  <p>Name: {{ name }}</p>
+  <p>Birth Year: {{ birthYear }}</p>
+  <p>Marital Status: {{ maritalStatus }}</p>
+
+
+</template>
